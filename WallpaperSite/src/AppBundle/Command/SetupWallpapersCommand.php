9ff779b4b36db85c0465e21dfb6c85a716a99ec0
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-17 11:54:24 +0200

    Added entitymanager, add images to database

--- WallpaperSite/src/AppBundle/Command/SetupWallpapersCommand.php
+++ WallpaperSite/src/AppBundle/Command/SetupWallpapersCommand.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Entity\Wallpaper;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,27 +14,43 @@ class SetupWallpapersCommand extends Command
      * @var string
      */
     private $rootDir;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
 
-    public function __construct(string $rootDir)
+    public function __construct(string $rootDir, EntityManagerInterface $em)
     {
         parent::__construct();
 
         $this->rootDir = $rootDir;
+        $this->em = $em;
     }
 
     protected function configure()
     {
         $this
             ->setName('app:setup-wallpapers')
-            ->setDescription('Grabs all the local wallpapers and creates a Wallpaper entity for each one.')
-        ;
+            ->setDescription('Grabs all the local wallpapers and creates a Wallpaper entity for each one.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $wallpapers = glob($this->rootDir . '/../web/images/*.*');
 
-        exit(\Doctrine\Common\Util\Debug::dump($wallpapers));
+        foreach ($wallpapers as $wallpaper) {
+
+            $wp = (new Wallpaper())
+                ->setFilename($wallpaper)
+                ->setSlug($wallpaper)
+                ->setWidth(2560)
+                ->setHeight(1600);
+
+            $this->em->persist($wp);
+
+        }
+
+        $this->em->flush();
 
         $output->writeln('Command result.');
     }
