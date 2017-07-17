78554e6512c485ae4a3b743cdb1c9bef093bed00
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-17 18:04:29 +0200

    add easy admin bundle

--- WallpaperSite/app/AppKernel.php
+++ WallpaperSite/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new AppBundle\AppBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
