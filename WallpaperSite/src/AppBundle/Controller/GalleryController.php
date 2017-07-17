8ebc86c2d59ccee590095f8aae3aad22f973984a
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-17 19:17:03 +0200

    add all images

--- WallpaperSite/src/AppBundle/Controller/GalleryController.php
+++ WallpaperSite/src/AppBundle/Controller/GalleryController.php
@@ -21,13 +21,26 @@ class GalleryController extends Controller
             'skyline4.jpg',
             'skyline5.jpg',
             'skyline6.jpg',
+            'm3.jpg',
+            'm31.jpg',
+            'm32.jpg',
+            'm33.jpg',
+            'm34.jpeg',
+            'm35.jpg',
+            'm36.jpg',
+            'shelby.jpeg',
+            'shelby1.jpg',
+            'shelby2.jpg',
+            'shelby3.jpg',
+            'shelby4.jpg',
+            'shelby5.jpg',
         ];
 
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $images,
             $request->query->getInt('page', 1)/*page number*/,
-            4/*limit per page*/
+            6/*limit per page*/
         );
 
         return $this->render('gallery/index.html.twig', [
