6941781035ab3a7b0785e3a16b5f60f41f3e54bb
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-18 12:40:12 +0200

    construct

--- WallpaperSite/src/AppBundle/Service/FileMover.php
+++ WallpaperSite/src/AppBundle/Service/FileMover.php
@@ -2,11 +2,23 @@
 
 namespace AppBundle\Service;
 
+use Symfony\Component\Filesystem\Filesystem;
+
 class FileMover
 {
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
     public function move($existingFilePath, $newFilePath)
     {
-        // do the move her
+        $this->filesystem->rename($existingFilePath, $newFilePath);
 
         return true;
     }
